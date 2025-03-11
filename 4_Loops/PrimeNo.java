8ec7af3d8999ec14edaef66c57dc696d4eac9f31
hahapratik <harshpoddar570@gmail.com>
2025-03-11 23:56:03 +0530

    comets

--- 4_Loops/PrimeNo.java
+++ 4_Loops/PrimeNo.java
@@ -1,18 +1,22 @@
-import java.util.Scanner;
+
+import java.util.Scanner; // Import the Scanner class for user input
 
 public class PrimeNo {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        Scanner sc = new Scanner(System.in); // Create a Scanner object for input
+        int n = sc.nextInt(); // Read an integer input from the user
 
-        boolean isPrime = true;
-        for( int i = 2 ; i <= 1; i++){
-            if (n%i == 0) {
-                isPrime=false;
+        boolean isPrime = true; // Assume the number is prime
+        for (int i = 2; i <= Math.sqrt(n); i++) { // Loop from 2 to the square root of n
+            if (n % i == 0) { // Check if n is divisible by i
+                isPrime = false; // If divisible, n is not prime
+                break; // Exit the loop as we found a divisor
             }
         }
-        if (isPrime==true) {
-            System.out.println("N is prime");
-        }else{System.out.println("N is not prime");}
+        if (isPrime == true) { // If isPrime is still true
+            System.out.println("N is prime"); // Print that n is prime
+        } else {
+            System.out.println("N is not prime"); // Otherwise, print that n is not prime
+        }
     }
 }
