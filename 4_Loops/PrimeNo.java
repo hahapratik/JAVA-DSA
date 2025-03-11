04f5a5e3e413dcbe6c38a7a8c47b7e99ef2fb5e8
hahapratik <harshpoddar570@gmail.com>
2025-03-11 23:53:56 +0530

    prime

---
+++ 4_Loops/PrimeNo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrimeNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        boolean isPrime = true;
+        for( int i = 2 ; i <= 1; i++){
+            if (n%i == 0) {
+                isPrime=false;
+            }
+        }
+        if (isPrime==true) {
+            System.out.println("N is prime");
+        }else{System.out.println("N is not prime");}
+    }
+}
