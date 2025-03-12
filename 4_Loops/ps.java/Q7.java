d0b013c0e8a54ea78a9b96ef4b91111b67120408
hahapratik <harshpoddar570@gmail.com>
2025-03-12 18:04:08 +0530

    Q7

---
+++ 4_Loops/ps.java/Q7.java
@@ -0,0 +1,12 @@
+
+public class Q7 {
+    public static void main(String[] args) {
+        // Loop from 0 to 4 (inclusive)
+        for(int i = 0; i < 5; i++) {
+            // Print "HELLO" to the console
+            System.out.println("HELLO");
+            // Increment i by 2
+            i += 2;
+        }
+    }
+}
