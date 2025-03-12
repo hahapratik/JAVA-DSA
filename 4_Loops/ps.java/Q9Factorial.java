0e44f96c053d39acf99b3c05dc87f5745041115a
hahapratik <harshpoddar570@gmail.com>
2025-03-12 18:40:59 +0530

    Q9

---
+++ 4_Loops/ps.java/Q9Factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; // Import the Scanner class for user input
+
+
+public class Q9Factorial {
+    public static void main(String[] args) {
+        int n, fact = 1; // Initialize variables n and fact
+        Scanner sc = new Scanner(System.in); // Create a Scanner object for input
+        {
+            System.out.print("Enter the number : "); // Prompt the user to enter a number
+            n = sc.nextInt(); // Read the integer input from the user
+
+            for (int i = 1; i <= n; i++) { // Loop from 1 to n
+                fact *= i; // Multiply fact by i in each iteration
+            }
+        }
+        System.out.println("FACTORIAL : " + fact); // Print the factorial of the number
+    }
+}
