0687fa978af0bab72d22b2a60f80a0427c50bed0
hahapratik <harshpoddar570@gmail.com>
2025-03-16 21:54:48 +0530

    1

--- 4_Loops/ps.java/Q10Table.java
+++ 4_Loops/ps.java/Q10Table.java
@@ -10,7 +10,7 @@ public class Q10Table {
             System.out.print("Enter the number you want table of: "); // Prompt the user to enter a number
             n = sc.nextInt(); // Read the number entered by the user and store it in the variable 'n'
 
-            for (int i = 1; i <= 10; i++) { // Loop from 1 to 10 to generate the multiplication table
+            for (int i = 1; i <= 10; i++) {// Loop from 1 to 10 to generate the multiplication table
                 // Print the multiplication table in the format "n * i = result"
                 // 'n' is the number entered by the user
                 // 'i' is the current number in the loop (from 1 to 10)
