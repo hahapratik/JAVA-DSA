44093f6f7bb6c8fceba178214ca72a8e3c04e49c
hahapratik <harshpoddar570@gmail.com>
2025-03-18 23:41:25 +0530

    okey

--- 4_Loops/DoWhile.java
+++ 4_Loops/DoWhile.java
@@ -1,9 +1,14 @@
 public class DoWhile {
     public static void main(String[] args) {
+        // Initialize the counter variable
         int n = 1;
-     do {
-        System.out.println("HELLO");
-        n++;
-     } while (n<=10);
+        
+        // Do-while loop to print "HELLO" 10 times
+        do {
+            // Print "HELLO" to the console
+            System.out.println("HELLO");
+            // Increment the counter variable
+            n++;
+        } while (n <= 10); // Continue the loop while n is less than or equal to 10
     }
 }
