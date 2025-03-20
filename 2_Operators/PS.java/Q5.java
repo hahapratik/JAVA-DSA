e82eb34ab9c135319de72b5c08d04ebb2132f217
hahapratik <harshpoddar570@gmail.com>
2025-03-20 23:41:35 +0530

    4

--- 2_Operators/PS.java/Q5.java
+++ 2_Operators/PS.java/Q5.java
@@ -1,12 +1,27 @@
 public class Q5 {
 
     public static void main(String[] args) {
+        // Initialize variables x and y
         int x = 10, y = 5;
 
+        // Calculate the first expression
+        // exp1 = y * (x / y + x / y)
+        // exp1 = 5 * (10 / 5 + 10 / 5)
+        // exp1 = 5 * (2 + 2)
+        // exp1 = 5 * 4
+        // exp1 = 20
         int exp1 = (y * (x / y + x / y));
+
+        // Calculate the second expression
+        // exp2 = y * x / y + y * x / y
+        // exp2 = 5 * 10 / 5 + 5 * 10 / 5
+        // exp2 = 50 / 5 + 50 / 5
+        // exp2 = 10 + 10
+        // exp2 = 20
         int exp2 = (y * x / y + y * x / y);
         
-        System.out.println(exp1);
-        System.out.println(exp2);
+        // Print the results of the expressions
+        System.out.println(exp1); // Output: 20
+        System.out.println(exp2); // Output: 20
     }
 }
