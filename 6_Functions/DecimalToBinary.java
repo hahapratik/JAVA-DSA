1991fdd409f3c2dee4f81a7599a44ced299333e5
hahapratik <harshpoddar570@gmail.com>
2025-04-03 20:24:24 +0530

    add DecimalToBinary class to convert decimal numbers to binary

---
+++ 6_Functions/DecimalToBinary.java
@@ -0,0 +1,15 @@
+public class DecimalToBinary {
+    public static void DecToBin (int n ){
+        int MyNum = n;
+        int pow = 0;
+        int binNum = 0;
+        while (n>0) {
+            int rem = n%2;
+            binNum= binNum+(rem* (int)Math.pow(10,pow));
+            pow ++;
+            n =n/2;
+        }System.out.println("Binary form of " + MyNum + " = " + binNum) ;
+    }public static void main(String[] args) {
+        DecToBin(15);
+    }
+}
