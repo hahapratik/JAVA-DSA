472e01bf0ae6bb51aa831aadd26086a4deb84890
hahapratik <harshpoddar570@gmail.com>
2025-04-05 21:13:57 +0530

    enhance Q1.java with comments for better clarity and user input prompts

--- 6_Functions/ps.java/Q1.java
+++ 6_Functions/ps.java/Q1.java
@@ -1,20 +1,36 @@
 
 import java.util.Scanner;
+// Importing the Scanner class for taking user input
 
 public class Q1 {
     
     public static void main(String[] args) {
-        int a,b,c;
+        int a, b, c; // Declaring three integer variables to store user input
+        
+        // Creating a Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
+        
+        // Prompting the user to enter the first number
         System.out.println("Enter the first number : ");
-        a = sc.nextInt();
+        a = sc.nextInt(); // Reading the first number
+        
+        // Prompting the user to enter the second number
         System.out.println("Enter the second number : ");
-        b = sc.nextInt();
+        b = sc.nextInt(); // Reading the second number
+        
+        // Prompting the user to enter the third number
         System.out.println("Enter the third number : ");
-        c = sc.nextInt();
-        int avg;
+        c = sc.nextInt(); // Reading the third number
+        
+        int avg; // Declaring a variable to store the average
+        
+        // Calculating the average of the three numbers
         avg = (a + b + c) / 3;
+        
+        // Printing the calculated average to the console
         System.out.println("The average of thic given Numbers is = " + avg);
+        
+        // Closing the Scanner object to release resources
         sc.close();
     }
 }
