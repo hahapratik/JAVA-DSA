1363b92d4e285b3ad49cc1d059417c560e9e741a
hahapratik <harshpoddar570@gmail.com>
2025-04-10 21:17:15 +0530

    nothing

--- 7_AdvancePatterns/SolidRhombus.java
+++ 7_AdvancePatterns/SolidRhombus.java
@@ -12,6 +12,7 @@ public class SolidRhombus {
             for (int j = 1; j <= n; j++) {
                 System.out.print("* "); // Print star followed by a space
             }
+            
             System.out.println(); // Move to the next line after each row
         }
     }
