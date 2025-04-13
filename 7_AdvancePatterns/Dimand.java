70d7bbb21284701e5b7499123271b5a62118eaaf
hahapratik <harshpoddar570@gmail.com>
2025-04-13 21:35:43 +0530

    Dimand

---
+++ 7_AdvancePatterns/Dimand.java
@@ -0,0 +1,31 @@
+public class Dimand {
+    public static void dimand(int n) {
+        // 1st half
+        for (int i = 1; i <= n; i++) {
+            // stars
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+             // stars
+             for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // 2nd half
+        for (int i = n; i >= n; i--) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+         // stars
+         for (int j = 1; j <= (2 * i) - 1; j++) {
+            System.out.print("*");
+        }
+    }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        dimand(5);
+    }
+}
