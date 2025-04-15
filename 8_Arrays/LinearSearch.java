b52d2e5704273fb9fa1ec1603115d2ed0d5f0aa2
hahapratik <harshpoddar570@gmail.com>
2025-04-15 10:51:16 +0530

    LL

---
+++ 8_Arrays/LinearSearch.java
@@ -0,0 +1,21 @@
+public class LinearSearch {
+    public static int linearSearch(int num[], int key) {
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int num[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
+        int key = 13;
+        int index = linearSearch(num, key);
+        if (index == -1) {
+            System.out.println("NOT FOUND");
+        } else {
+            System.out.println("Key is at Index " + index);
+        }
+    }
+}
