ccda8e64656095688789dd7d9a5b187a5b420fd5
hahapratik <harshpoddar570@gmail.com>
2025-04-17 14:31:28 +0530

    REVERSE

---
+++ 8_Arrays/Reserve.java
@@ -0,0 +1,18 @@
+public class Reserve {
+    public static void reserve(int number[]){
+        int first = 0 , last = number.length-1;
+        while (first<last) {
+            int temp =number[last];
+            number[last]= number[first];
+            number[first] = temp;
+            first++;
+            last--;
+        }
+    }
+    public static void main(String[] args) {
+        int number[] = {2,4,6,8,10,12,14};
+        reserve(number);
+        for(int i = 0; i<number.length; i++){
+        System.out.print(number[i]+" ");}
+    }
+}
