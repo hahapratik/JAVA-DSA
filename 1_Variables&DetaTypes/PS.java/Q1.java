f113ab2d65607f0c258af8fbecee750311514010
hahapratik <harshpoddar570@gmail.com>
2025-05-18 14:43:32 +0530

    nothing

--- 1_Variables&DetaTypes/PS.java/Q1.java
+++ 1_Variables&DetaTypes/PS.java/Q1.java
@@ -2,24 +2,21 @@ import java.util.Scanner;
 
 public class Q1 {
     public static void main(String[] args) {
-        // Question 1: In a program, input 3 numbers : A, B and C. You have to output the average
-        // of these 3 numbers.
-        float a, b, c, average;
-        Scanner sc = new Scanner(System.in);
-        {
-            System.out.print("Enter the value of a : ");
-            a = sc.nextFloat();
-            System.out.print("Enter the value of b : ");
-            b = sc.nextFloat();
-            System.out.print("Enter the value of c : ");
-            c = sc.nextFloat();
-            
-            // now apply the foemula of average
-            average = (a + b + c) / 3;
-            
-            System.out.print("The Average of three numbers is : ");
-            System.out.print(average);
-            sc.close();
-        }
+        Scanner sc = new Scanner(System.in); // Create a Scanner object for user input
+
+        System.out.print("Enter thr value of a : "); // Prompt user to enter value of a
+        float a = sc.nextInt(); // Read integer input and store in float variable 'a'
+
+        System.out.print("Enter thr value of b : "); // Prompt user to enter value of b
+        float b = sc.nextInt(); // Read integer input and store in float variable 'b'
+
+        System.out.print("Enter thr value of c : "); // Prompt user to enter value of c
+        float c = sc.nextInt(); // Read integer input and store in float variable 'c'
+
+        float Average = (a + b + c) / 3; // Calculate the average of a, b, and c
+
+        System.out.println(" Average is " + Average); // Display the calculated average
+
+        sc.close(); // Close the Scanner to free resources
     }
 }
