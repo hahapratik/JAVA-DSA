36ea356b290485fd749603201db9e5bf5b4a4ad6
hahapratik <harshpoddar570@gmail.com>
2025-05-19 10:43:37 +0530

    revision

--- REVISION/pattern.java
+++ REVISION/pattern.java
@@ -1,12 +1,34 @@
 package REVISION;
 
+import java.util.Scanner;
+
 public class pattern {
-   public static void main(String[] args) {
-    for(int i =1;i<=5; i++){
-        for(int j =1; j<=5-i; j++){
-            System.out.print("+");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the  number : ");
+        // int a = sc.nextInt();
+        // // System.out.print("Enter the Second number : ");
+        // // int b = sc.nextInt();
+        // // System.out.print("Enter the Third number : ");
+        // // int c = sc.nextInt();
+        // if (a < 0) {
+        //     System.out.println("NEGETIVE");
+        // }
+        // else if (a == 0) {
+        //     System.out.println("The number is Zero");
+        // } else {
+        //     System.out.println("POSITIVE");
+        // }
+        double temp = 103.5;
+        System.out.println("Enter your temp");
+        double tempt = sc.nextDouble();
+    
+        if (tempt > 100) {
+            System.out.println("You have fever");
+        }else{
+            System.out.println("you don't have fever");
         }
-        System.out.println();
+
     }
-   }
+
 }
