f21966158259daa20bfacc913dfb9899d36d1502
hahapratik <harshpoddar570@gmail.com>
2025-07-13 14:01:30 +0530

    Refactor Q2.java to improve output prompts and streamline area calculation for a square

--- 1_Variables&DetaTypes/PS.java/Q2.java
+++ 1_Variables&DetaTypes/PS.java/Q2.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
 
 public class Q2 {
-    // Question 2: In a program, input the side of a square. You have to output the area of the square.
+    // Question 2: In a program, input the side of a square. You have to output the
+    // area of the square.
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // Create a Scanner object to read input from the user
-
-        System.out.print("Enter the side of a SQUARE : "); // Prompt the user to enter the side of the square
-
-        int side = sc.nextInt(); // Read the integer input for the side of the square
-
-        int area = side * side; // Calculate the area of the square (side * side)
-
-        System.out.println("The area of Square with given side is " + area); // Output the calculated area to the user
+        System.out.println("Enter the sides of a Square = ");
+        int sides = sc.nextInt();
+        int area = sides * sides;
+        System.out.println("Area = " + area);
         sc.close();
     }
-    
 }
