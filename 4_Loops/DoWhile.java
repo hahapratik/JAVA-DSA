e77e6ac72ba3aef7d89165c4bf0cc7cc63fa1551
hahapratik <harshpoddar570@gmail.com>
2025-09-10 12:28:17 +0530

    Refactor loop implementations in DoWhile.java, ForLoop.java, and While.java for clarity; remove commented-out code and update loop logic.

--- 4_Loops/DoWhile.java
+++ 4_Loops/DoWhile.java
@@ -1,14 +1,21 @@
 public class DoWhile {
     public static void main(String[] args) {
         // Initialize the counter variable
-        int n = 1;
+        // int n = 1;
         
-        // Do-while loop to print "HELLO" 10 times
-        do {
-            // Print "HELLO" to the console
-            System.out.println("HELLO");
-            // Increment the counter variable
-            n++;
-        } while (n <= 10); // Continue the loop while n is less than or equal to 10
+        // // Do-while loop to print "HELLO" 10 times
+        // do {
+        //     // Print "HELLO" to the console
+        //     System.out.println("HELLO");
+        //     // Increment the counter variable
+        //     n++;
+        // } while (n <= 10); // Continue the loop while n is less than or equal to 
+        int i = 1;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<=10);
+    
     }
 }
--- 4_Loops/ForLoop.java
+++ 4_Loops/ForLoop.java
@@ -14,8 +14,8 @@ public class ForLoop {
     //     }
     // }
     public static void main(String[] args){
-for(int i = 1; i<=5; i++){
-    System.out.println("HELLO");
+for(int i = 0; i<=10; i++){
+    System.out.println(i);
 }
     }
 }
