3461f42b49cd5186398d8b50abbe83077d5e7a1a
hahapratik <harshpoddar570@gmail.com>
2025-09-21 11:32:40 +0530

    Remove unused classes and methods: delete BinaryToDecimal.java, BinomialCofficient.java, DecimalToBinary.java, FactorialFunction.java, FunctionOverloading.java, FunctionOverloading2.java, Multiplication.java, PrimeOrNot.java, PrimeTillN.java, PrimeUsingMath.java, Sum.java, Sum2.java, Temp.java, TempFunction.java, hello.java, MathMethods.java, Q1.java, Q2.java, Q3.java, Q4.java; clean up pattern.java and add basic input handling in hello_function.java.

---
+++ 6_Functions/hello_function.java
@@ -0,0 +1,8 @@
+public class hello_function {
+    public static void hello (){
+        System.out.println("HELLO WORLD");
+    }
+    public static void main(String[] args) {
+        hello();
+    }
+}
