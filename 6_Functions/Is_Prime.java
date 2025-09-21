ff817a227c3d2dbba4b7983552ff266d9f47c4cf
hahapratik <harshpoddar570@gmail.com>
2025-09-21 19:43:40 +0530

    Add Is_Prime.java with implementation of prime number checking and main method for testing

---
+++ 6_Functions/Is_Prime.java
@@ -0,0 +1,28 @@
+public class Is_Prime {
+    // public static boolean isPRime(int n) {
+    // if(n==2){
+    // return true;
+    // }
+    // boolean isPRime = true;
+    // for (int i = 2; i <= n - 1; i++) {
+    // if (n % i == 0) {
+    // isPRime = false;
+    // }
+    // }
+    // return isPRime;
+    // }
+    // optimied way
+    public static boolean isPRime(int n) {
+        if (n == 2) {
+            return true;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            boolean isPRime = false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPRime(7));
+    }
+}
