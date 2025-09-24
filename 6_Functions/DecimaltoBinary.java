cf20ebaf5318f09f36d42e207024cd2f77f432b4
hahapratik <harshpoddar570@gmail.com>
2025-09-24 09:38:41 +0530

    Add DecimaltoBinary.java with implementation of decimal to binary conversion and main method for testing

---
+++ 6_Functions/DecimaltoBinary.java
@@ -0,0 +1,17 @@
+public class DecimaltoBinary {
+    public static void DecToBin(int n){
+        int mynum = n;
+        int pow = 0;
+        int binNum = 0;
+        while(n>0){
+            int rem = n%2;
+            binNum = binNum+(rem * (int)Math.pow(10, pow));
+            pow++;
+            n = n/2;
+        }
+        System.out.println("Binary form of " + mynum + "= " +binNum);
+    }
+    public static void main(String[] args) {
+        DecToBin(7);
+    }
+}
