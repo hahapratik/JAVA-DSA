ba7077f4f63131c04c71a7e48157f8e3caf4343d
hahapratik <harshpoddar570@gmail.com>
2025-09-24 11:38:23 +0530

    Add q2.java with implementation of isEven method and main method for testing

---
+++ 6_Functions/ps/q2.java
@@ -0,0 +1,16 @@
+package ps;
+
+// Write a method named isEven that accepts an int argument. The method
+// should return true if the argument is even, or false otherwise. Also write a program to test your
+// method.
+public class q2 {
+    public static boolean isEven(int n) {
+        if ( n % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+       System.out.println( isEven(7));;
+    }
+}
